e79557afe7c07ba8a5ec9600845429c751a6badd
ikucuk <incikucuk98@gmail.com>
2024-07-13 14:26:07 +0300

    department-service rest api completed

---
+++ department-service/src/main/java/com/javaguides/department_service/controller/DepartmentController.java
@@ -0,0 +1,31 @@
+package com.javaguides.department_service.controller;
+
+import com.javaguides.department_service.dto.DepartmentDto;
+import com.javaguides.department_service.entity.Department;
+import com.javaguides.department_service.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+    private DepartmentService departmentService;
+
+    //Build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartmentDto = departmentService.saveDepartmentDto(departmentDto);
+
+        return new ResponseEntity<>(savedDepartmentDto, HttpStatus.CREATED);
+    }
+
+    //Build get department rest api
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
+       DepartmentDto departmentDto =  departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto,HttpStatus.OK);
+    }
+}
---
+++ department-service/src/main/java/com/javaguides/department_service/service/DepartmentService.java
@@ -0,0 +1,11 @@
+package com.javaguides.department_service.service;
+
+import com.javaguides.department_service.dto.DepartmentDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DepartmentService {
+    DepartmentDto saveDepartmentDto(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentByCode(String code);
+}
